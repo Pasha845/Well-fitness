bbd37b2f881dc2971a970036b2ec6c798b988e1b
Kiomi Saito <pasha.gorulev@gmail.com>
2023-10-23 19:58:06 +0300

    60%

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -9,24 +9,30 @@ const heroSwiper = new Swiper('.hero__slider', {
   }
 });
 
-const newsSwiper = new Swiper('.news__slider', {
-  loop: true,
+const stockSwiper = new Swiper('.stock__slider', {
   slidesPerView: 4,
+  grid: {
+    rows: 1
+  },
+  spaceBetween: 30,
   navigation: {
-    nextEl: ".news__next",
-    prevEl: ".news__prev"
+    nextEl: ".stock__next",
+    prevEl: ".stock__prev"
   },
   scrollbar: {
     hide: true,
   }
 });
 
-const stockSwiper = new Swiper('.stock__slider', {
-  loop: true,
+const newsSwiper = new Swiper('.news__slider', {
   slidesPerView: 4,
+  grid: {
+    rows: 1
+  },
+  spaceBetween: 30,
   navigation: {
-    nextEl: ".stock__next",
-    prevEl: ".stock__prev"
+    nextEl: ".news__next",
+    prevEl: ".news__prev"
   },
   scrollbar: {
     hide: true,
